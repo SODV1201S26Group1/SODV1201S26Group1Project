202cbd14d4f3a7968f7199b3c529d9972ec75b4b
frimpomaahadarkwahyaa <frimpomaahadarkwahyaa@gmail.com>
2026-06-03 19:04:06 -0600

    Add POST route for user registration

--- server.js
+++ server.js
@@ -6,6 +6,12 @@ const users = [];
 app.use(express.json());
 app.use(express.static('public'));
 
-app.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.post('/register', (req, res) => {
+  const {name, phone, email, password, role } = req.body;
+  users.push({ name, phone, email, password,role });
+  res.json({ success: true, message: 'User registered!' });
 });
+
+   app.listen(3000, () => {
+   console.log('Server running on port 3000');
+    });
