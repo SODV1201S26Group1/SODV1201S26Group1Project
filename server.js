41c28a98bfabc62890072838b7d33a2c83f888b7
frimpomaahadarkwahyaa <frimpomaahadarkwahyaa@gmail.com>
2026-06-03 19:16:47 -0600

    Add POST route for login

--- server.js
+++ server.js
@@ -7,11 +7,21 @@ app.use(express.json());
 app.use(express.static('public'));
 
 app.post('/register', (req, res) => {
-  const {name, phone, email, password, role } = req.body;
-  users.push({ name, phone, email, password,role });
-  res.json({ success: true, message: 'User registered!' });
+const {name, phone, email, password, role } = req.body;
+users.push({ name, phone, email, password,role });
+res.json({ success: true, message: 'User registered!' });
 });
 
-   app.listen(3000, () => {
-   console.log('Server running on port 3000');
-    });
+app.post('/login', (req, res) => {
+    const {email, password } = req.body;
+    const user = users.find(u => u.email === email && u.password === password);
+    if (user) {
+        res.json({ success: true, role: user.role });
+    } else {
+        res.json({ success: false, message: 'Invalid credentials' });
+    }
+});
+
+app.listen(3000, () => {
+console.log('Server running on port 3000');
+});
